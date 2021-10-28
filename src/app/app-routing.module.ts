acfe8ef540a5ced5ea57d7efebcd214f3182bc3f
AnaëlR <hey@anael-rodet.fr>
2021-10-28 13:40:25 +0200

    Create pages and import bulma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePagesComponent} from "./pages/home-pages/home-pages.component";
+import {FormPagesComponent} from "./pages/form-pages/form-pages.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePagesComponent
+  },
+  {
+    path: 'form',
+    component: FormPagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { FormPagesComponent } from './pages/form-pages/form-pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePagesComponent,
+    FormPagesComponent
   ],
   imports: [
     BrowserModule,
